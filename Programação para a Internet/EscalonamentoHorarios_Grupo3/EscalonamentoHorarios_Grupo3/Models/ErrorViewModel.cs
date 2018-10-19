9f2226a7326ff4e5a4d9f1cba7a49e5db9cfa9db
Pedro Fiuza Sanches <32898392+PedroSanches10@users.noreply.github.com>
2018-10-19 12:14:57 +0100

    Projeto Visual Studio
    Criação do projeto no Visual Studio 2017.

---
+++ Programação para a Internet/EscalonamentoHorarios_Grupo3/EscalonamentoHorarios_Grupo3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EscalonamentoHorarios_Grupo3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
