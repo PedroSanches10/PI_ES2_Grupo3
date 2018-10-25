d7410338fae1737d3bd02816caca28a2bacb0b6c
Micael Santos <pelinha_12@hotmail.com>
2018-10-25 02:30:58 +0100

    Algumas alteraçoes
    Criaçao do horario, pag para o horario, contem alguns erros.

--- Programação para a Internet/EscalonamentoHorarios_Grupo3/EscalonamentoHorarios_Grupo3/Controllers/Enfermeiro.cs
+++ Programação para a Internet/EscalonamentoHorarios_Grupo3/EscalonamentoHorarios_Grupo3/Controllers/Enfermeiro.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EscalonamentoHorarios_Grupo3.Models;
+
 namespace EscalonamentoHorarios_Grupo3.Controllers
+
 {
-    public class Enfermeiro
+    public class Enfermeiro: Controller
     {
-        public Enfermeiro()
+        public IActionResult Enfermeiro()
         {
+            return View();
         }
     }
 }
